72ecd72b70c3e6f6c378d43b8d8b895cbe74d7ea
okaka8080 <lg.star1208@gmail.com>
2024-04-21 13:32:17 +0900

    add:id指定ルーム作成機能の追加 #8

--- lib/alternativeServer_web/channels/room_channel.ex
+++ lib/alternativeServer_web/channels/room_channel.ex
@@ -3,12 +3,12 @@ defmodule AlternativeServerWeb.RoomChannel do
   require Logger
 
   def join("room:lobby", _message, socket) do
-
     {:ok, socket}
   end
 
-  def join("room:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  def join("room:" <> private_room_id, _params, socket) do
+    {:ok, socket}
+    # {:error, %{reason: "unauthorized"}}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
